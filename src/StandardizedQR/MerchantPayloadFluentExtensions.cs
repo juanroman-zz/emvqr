b0de182d40be9cf330a2af9ef188104fb4c6989a
Juan Roman Escamilla <juanroman@hotmail.com>
2018-06-26 20:30:23 -0500

    Adding helper extensions to interpret Specifiaction's "***" and "AME" keywords.

--- src/StandardizedQR/MerchantPayloadFluentExtensions.cs
+++ src/StandardizedQR/MerchantPayloadFluentExtensions.cs
@@ -1,9 +1,76 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace StandardizedQR
 {
     public static class MerchantPayloadFluentExtensions
     {
+        /// <summary>
+        /// Indicates if the client application should the prompt for a bill number.
+        /// </summary>
+        public static bool ShouldPromptForBillNumber(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.BillNumber);
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for a mobile number.
+        /// </summary>
+        public static bool ShouldPromptForMobileNumber(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.MobileNumber);
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for a store label.
+        /// </summary>
+        public static bool ShouldPromptForStoreLabel(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.StoreLabel);
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for a loyalty number.
+        /// </summary>
+        public static bool ShouldPromptForLoyaltyNumber(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.LoyaltyNumber);
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for a reference label.
+        /// </summary>
+        public static bool ShouldPromptForReferenceLabel(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.ReferenceLabel);
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for a customer label.
+        /// </summary>
+        public static bool ShouldPromptForCustomerLabel(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.CustomerLabel);
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for the terminal label.
+        /// </summary>
+        public static bool ShouldPromptForTerminalLabel(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.TerminalLabel);
+
+        /// <summary>
+        /// Indicates if the client application should automaticaly add the consumer address to the transaction.
+        /// </summary>
+        public static bool ShouldAddConsumerAddress(this MerchantPayload merchantPayload) =>
+            null != merchantPayload.AdditionalData && merchantPayload.AdditionalData.AdditionalConsumerDataRequest.Contains("A");
+
+        /// <summary>
+        /// Indicates if the client application should automaticaly add the consumer mobile number to the transaction.
+        /// </summary>
+        public static bool ShouldAddConsumerMobileNumber(this MerchantPayload merchantPayload) =>
+            null != merchantPayload.AdditionalData && merchantPayload.AdditionalData.AdditionalConsumerDataRequest.Contains("M");
+
+        /// <summary>
+        /// Indicates if the client application should automaticaly add the consumer email to the transaction.
+        /// </summary>
+        public static bool ShouldAddConsumerEmail(this MerchantPayload merchantPayload) =>
+            null != merchantPayload.AdditionalData && merchantPayload.AdditionalData.AdditionalConsumerDataRequest.Contains("E");
+
+        /// <summary>
+        /// Indicates if the client application should the prompt for purpose of the transaction.
+        /// </summary>
+        public static bool ShouldPromptForPurposeOfTransaction(this MerchantPayload merchantPayload) =>
+            0 == StringComparer.InvariantCultureIgnoreCase.Compare("***", merchantPayload.AdditionalData?.PurposeOfTransaction);
+
         public static MerchantPayload WithTransactionAmount(this MerchantPayload merchantPayload, decimal transactionAmount)
         {
             merchantPayload.TransactionAmount = transactionAmount;
@@ -39,7 +106,7 @@ namespace StandardizedQR
             return merchantPayload.WithUnreservedTemplate(80, globalUniqueIdentifier, contextSpecificData);
         }
 
-        public static MerchantPayload WithUnreservedTemplate(this MerchantPayload merchantPayload, int id, string globalUniqueIdentifier, Dictionary<int,string> contextSpecificData)
+        public static MerchantPayload WithUnreservedTemplate(this MerchantPayload merchantPayload, int id, string globalUniqueIdentifier, Dictionary<int, string> contextSpecificData)
         {
             merchantPayload.UnreservedTemplate = new MerchantUnreservedDictionary
             {
